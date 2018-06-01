04244585dc9a85849b6185b756914abdb40ebc67
mvdr <m.ruijt@niposoftware.com>
2018-06-01 11:04:54 +0200

    Initial setup

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('buy-signals', { path: '/' });
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'crypto-signals',
+    podModulePrefix: 'crypto-signals/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
